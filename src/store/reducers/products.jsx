997350b8c649875048f795246ed1e6c912acee9e
Elena Kochegarova <elenakoch122@ya.ru>
2024-05-03 10:03:25 +0700

    added localStorage

--- src/store/reducers/products.jsx
+++ src/store/reducers/products.jsx
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { products } from "../../products";
 
-const initialState = {
+const initialState = JSON.parse(localStorage.getItem('products')) || {
   products,
   basketProducts: [],
   count: 0,
@@ -16,18 +16,21 @@ export const productsSlice = createSlice({
       state.basketProducts.push(action.payload);
       state.count = state.basketProducts.length;
       state.sum += action.payload.price;
+      localStorage.setItem('products', JSON.stringify(state));
     },
 
     removeProductFromBasket: (state, action) => {
       state.basketProducts = state.basketProducts.filter(prod => prod.idUnic !== action.payload);
       state.count = state.basketProducts.length;
       state.sum = state.basketProducts.reduce((acc, product) => acc + product.price, 0);
+      localStorage.setItem('products', JSON.stringify(state));
     },
 
     removeAllProducts: (state) => {
       state.basketProducts = [];
       state.count = 0;
       state.sum = 0;
+      localStorage.removeItem('products');
     },
   }
 });
