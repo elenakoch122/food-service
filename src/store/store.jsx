2a07c467f2520c8f169ddb17cf027bd053dd3a29
Elena Kochegarova <elenakoch122@ya.ru>
2024-05-07 16:57:44 +0700

    refactored authentication

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./reducers/products";
+import authReducer from "./reducers/auth";
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
+    auth: authReducer,
   }
 });
